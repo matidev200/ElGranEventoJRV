43f39b5e2a263f3b5fab5734ae075d4910a9841b
babinobass <69868198+babinobass@users.noreply.github.com>
2021-12-04 07:50:13 -0300

    home casi finalizado, footer coon sombra y btn primario con primeros css

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 import "./Buttons.scss"
 
-export const ButtonPrimary = ({children, onClick}) => {
+export const ButtonPrimary = ({ children, onClick, className }) => {
 
     return (
-        <>
+        <div className={"btnPrimary " + (className)}>
             <button onClick={onClick}>{children}</button>
-        </>
+        </div>
     )
 }
 
-export const ButtonSecondary = ({children}) => {
+export const ButtonSecondary = ({ children }) => {
     return (
         <>
             <button>{children}</button>
--- src/messages/messages.js
+++ src/messages/messages.js
@@ -1,5 +1,9 @@
 export const messages = {
     es: {
-        "Home.Title": "Ejemplo de como se usa",
+        "Home.versiculo": "Se oyó un grito, se despertaron y encendieron sus lámparas...",
+        "Home.cap": "Mateo 25",
+        "Home.title": "UN EVENTO PARA ADORAR A DIOS",
+        "Home.content": "Dios nos llamó a levantar un altar de adoración el 18 de diciembre. Nos es un día de música ni de músicos, es el día de la iglesia para derramar el  corazón en honra a nuestro único amor, Jesús. Te invitamos a unirte a nosotros para este día tan especial. Trae tu mejor adoración, tu corazón.",
+        "Home.fecha": "El eventó será realizdo el sabado 18/12",
     }
 }
