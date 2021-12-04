aa3d9058fcc4650d602444ff271f0e94d37c2772
babinobass <69868198+babinobass@users.noreply.github.com>
2021-12-03 21:16:37 -0300

    push rapido

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React, { useEffect } from 'react';
 import '../src/public/style/base.scss';
 import { IntlProvider } from "react-intl";
+import NavBar from './components/NavBar/NavBar';
 import { messages as allMessages } from './messages/messages';
+import Home from "./views/Home/Home"
 import Register from './views/Register/Register';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
-import {obtenerDatos} from './service/firebaseService'
+import { obtenerDatos } from './service/firebaseService'
 
 const App = () => {
   const currentLocale = "es";
@@ -22,12 +24,13 @@ const App = () => {
   return (
     <IntlProvider locale={currentLocale} messages={messages}>
       <div className="App">
-
-          <Router>
-            <Routes>
-              <Route path="/Registro" element={<Register />}/>
-            </Routes>
-          </Router>
+        <NavBar />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Registro" element={<Register />} />
+          </Routes>
+        </Router>
       </div>
     </IntlProvider>
   );
