2962929357cc3745fe8133be39f8e27969f4bf49
Faculiao <facutoloza2005@gmail.com>
2021-12-05 02:19:55 -0300

    Arreglando la información del Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,15 +8,15 @@ const Footer = () => {
             
                 <ul className="footer__ul">
                     <li className="footer__ul-title">Redes Sociales</li>
-                    <li><a href="https://www.instagram.com" target="_BLANK" rel="noreferrer">Instagram</a></li>
-                    <li ><a href="https://www.facebook.com" target="_BLANK" rel="noreferrer">Facebook</a></li>
-                    <li ><a href="https://www.youtube.com" target="_BLANK" rel="noreferrer">Youtube</a></li>
+                    <li><a href="https://www.instagram.com/iglesiajrvcentral_escobar/" target="_BLANK" rel="noreferrer">Instagram</a></li>
+                    <li ><a href="https://www.facebook.com/jesucristo.reydevida" target="_BLANK" rel="noreferrer">Facebook</a></li>
+                    <li ><a href="https://www.youtube.com/c/JesucristoReydeVidaCENTRAL" target="_BLANK" rel="noreferrer">Youtube</a></li>
                 </ul>
 
                 <ul className="footer__ul">
                     <li className="footer__ul-title">Contacto</li>
-                    <li >0348-4313-655</li>
-                    <li >0348-4313-655</li>
+                    <li>0348-4313-655</li>
+                    <li><a href="mailto:jrvcentralescobar@gmail.com" target="_blank" rel="noreferrer">Soporte</a></li>
                     
                 </ul>
            
@@ -24,7 +24,7 @@ const Footer = () => {
             
                 <ul className="footer__ul">
                     <li className="footer__ul-title">Dirección</li>
-                     <li >Dr. Travi  697</li>
+                     <li >Dr. Travi  697 <br />  Belén de Escobar</li>
                 </ul>
 
                 
