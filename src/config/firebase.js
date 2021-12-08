f8056511de2d99edaa9ce944627aa3b18fa92293
Matias <matiasdv1@hotmail.com>
2021-12-08 17:12:14 -0300

    Base de datos definitiva (producción)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_DEV_APIKEY,
-  authDomain: process.env.REACT_APP_DEV_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_DEV_PROJECTID,
-  storageBucket: process.env.REACT_APP_DEV_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_DEV_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_DEV_APPID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 
